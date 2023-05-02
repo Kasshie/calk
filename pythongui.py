57ff5b5bb3a9bd82f3c5237243de077299dc550b
Kasshie <maximelegaregagnon22@gmail.com>
2023-05-01 21:07:52 -0400

    stuff

--- pythongui.py
+++ pythongui.py
@@ -1,16 +1,32 @@
-import PySimpleGUI as sg
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+import invoke as inv
+import ctypes
+import pathlib
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
 
-# Create the window
-window = sg.Window("Demo", layout)
+root = tk.Tk()
+apps = []
 
-# Create an event loop
-while True:
-    event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
-        break
+#open the file dialog
+#def bawb():
 
-window.close()
+#    for widget in canvas.winfo_children():
+#        widget.destroy()
+
+#    filename = filedialog.askopenfilename(initialdir='/', title="bawb", filetypes=(("executables", "*.exe"), ("allfiles", "*.*")))
+#    apps.append(filename)
+#   print(filename)
+#    for app in apps:
+#        label = tk.Label( canvas, text=app, bg="grey")
+#        label.pack()
+
+
+canvas = tk.Canvas(root, height=700, width=700, bg="#272727")
+canvas.pack()
+
+calk = tk.Button(root, text="calk", fg="white", bg="#000000", command=bawb)
+calk.pack()
+
+root.mainloop()
